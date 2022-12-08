8b91d5946304db8f3f95e3f9e00b48d4a24fd449
Sigit <cigetbudi@gmail.com>
2022-12-09 01:08:34 +0700

    Add SetupRouter func Handler

--- api/gin.go
+++ api/gin.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/", home)
+	r.GET("/api/v1/articles/:id", getArticle)
+	r.GET("/api/v1/articles", getArticles)
+	r.POST("/api/v1/articles", postArticle)
+	r.PUT("/api/v1/articles/:id", putArticle)
+	r.DELETE("/api/v1/articles/:id", deleteArticle)
+	return r
+}
+
 func home(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Selamat datang di API gin-gorm article",
